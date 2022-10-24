82387ac89be3dafc1a68b8e32039328c834cfa3f
Dansky <lykimphong2001@example.com>
2022-10-24 17:52:02 +0700

    product(cont.)

--- views/product_detail.php
+++ views/product_detail.php
@@ -20,32 +20,35 @@ if (Application::isGuest()) {
                     <div class="col-md-12 col-lg-6 product-detail-right">
                         <div class="product-detail-name"><?php echo $params['product']->name ?></div>
                         <div class="product-detail-name-footer" >
-                        <?php echo $params['product']->year ?> <?php echo $params['product']->duration ?> min
-                        </div>
-                        <div class="inner-content text-center">
-                            <div class="c100 p50 big left green">
-                                <span><?php echo $params['product']->rating ?>%</span>
-                                <div class="slice">
-                                    <div class="bar"></div>
-                                    <div class="fill"></div>
+                        <ul class="auto actions" > 
+                            <li> <?php echo $params['product']->getCategory() ?> </li>
+                            <li> <?php echo $params['product']->year ?> </li>
+                            <li> <?php echo $params['product']->duration ?> min </li>
+</ul>    
+                    </div>
+                        <ul class="auto actions">
+                            <li class="chart">
+                            <div class="consensus details">
+                                <div class="outer_ring">
+                                <div class="user_score_chart" data-percent="0" data-track-color="#204529" data-bar-color="#21d07a">
+                                    <div class="percent">
+                                        <span class="icon"><?php echo $params['product']->rating ?>%</span>
+                                    </div>
+                                <canvas height="75" width="75" style="height: 60px; width: 60px;"></canvas></div>
                                 </div>
                             </div>
-                        </div>
+                            <div class="text">User<br>Score</div>
+                            </li>
+                        </ul>
                         <div class="product-detail-footer">
-                        <div class="product-detail-description" >
-                           Genre: <?php echo $params['product']->getCategory() ?>
-                        </div>
                         <div class="product-detail-description">
-                            <?php echo $params['product']->description ?>  
+                        <div class="text">Overview </div> <br> <?php echo $params['product']->description ?>  
                         </div>
                         <div class="product-detail-description" style="font-weight: bold;">
                            Director: <?php echo $params['product']->director ?>
                         </div>
                         <div class="product-detail-description" style="font-weight: bold;">
                            Stars: <?php echo $params['product']->stars ?>
-                        </div>
-                        <div class="product-detail-description">
-                           Rating: <?php echo $params['product']->rating ?>%
                         </div>
                         <div><span
                                     class="price"> Price: <?php echo number_format($params['product']->price, 0, ',', '.') ?></span>đ
@@ -95,6 +98,7 @@ if (Application::isGuest()) {
 
 <script src="/js/product_detail.js"></script>
 <script>
+
 <?php
     if ($params['addToCart']) {
         echo "var toastTrigger = document.getElementById('liveToastBtn')
@@ -104,3 +108,4 @@ if (Application::isGuest()) {
     }
     ?>
 </script>
+
